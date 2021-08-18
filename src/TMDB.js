387e4553c493a2b8228df308eb599210ddb83c49
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-18 20:30:41 -0300

    Adicionando a lista de filmes da Home do Site

--- src/TMDB.js
+++ src/TMDB.js
@@ -4,3 +4,61 @@
 //Os dados dos requests, usando um banco de dados feito por mim ou de outra API, eu apenas
 //PReciso editar este arquivo, pois ele irá centralizar tudo, tornando a minha vida muito
 //Mais fácil e deixando o desenvolvimento mais prático e organizado
+const API_KEY   = 'f42a31a6ad2f581ff02e34b50a18fa17'; //Chave da nossa API
+const API_BASE = 'https://api.themoviedb.org/3'; //Base dos dados que iremos usar da API, encontradas nesse URL
+
+/*
+-originais Netflix  
+-recomendados(trending)
+-em alta(top rated)
+-ação
+-terror
+-romance
+-comédia
+-documentários
+Cada informação desse generos vem da base, vai ser uma consulta diferente na API
+*/
+
+export default {
+getHomeList : async () => {
+    return[
+        {
+            slug: 'Originals';
+            title: "Originais Netflix",
+            itens : []
+        },
+        {
+            slug: 'trending',
+            title: "Recomendados para você",
+            itens : []
+        },
+        {
+            slug: 'top-rated',
+            title: "Em Alta",
+            itens : []
+        },
+        {
+            slug: 'ação',
+            title: "Ação",
+            itens : []
+        },
+        {
+            slug: 'terror',
+            title: "Terror",
+            itens : []
+        },
+        {
+            slug: 'comédia',
+            title: "Comédia",
+            itens : []
+        },
+        {
+            slug: 'romance',
+            title: "Romance",
+            itens : []
+        },
+        
+        
+    ];
+}
+}
