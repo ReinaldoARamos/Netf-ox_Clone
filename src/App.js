16d18d2b1a9f19e380faa50d8f0f462fdab7b997
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-18 20:06:09 -0300

    Criando um arquivo para centralizar os requests da API, explicação nos comentarios do arquivo SRC/TMDB.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { div } from 'prelude-ls';
+import React from 'react';
+
+export default () => {
+return (
+  <div>
+    Uatiman
+  </div>
+)
+
+}
---
+++ src/TMDB.js
@@ -0,0 +1,6 @@
+//Estou criando este arquivo para centralizar os requests que eu irei pegar da API TMDB, 
+//É melhor criar um arquivo assim pois todas as informações recebidas pelos requests
+//ficarão aqui, e não espalhadas pelos arquivos ao longo do APP, dessa forma, se algum dia eu for trocar
+//Os dados dos requests, usando um banco de dados feito por mim ou de outra API, eu apenas
+//PReciso editar este arquivo, pois ele irá centralizar tudo, tornando a minha vida muito
+//Mais fácil e deixando o desenvolvimento mais prático e organizado
