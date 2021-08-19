983d70c4447b9b548337c844404a11db4755db65
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-19 20:17:48 -0300

    Criando o Use Effect no App.Js, que recebe a HomeList com os dados da API, resolvendo alguns problemas de sintaxe no TBMDB, o problema agora é que ele retorna como api_key invalida, tenho que dar uma olhada

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import React from "react";
+import { list } from "postcss";
+import React, { useEffect } from "react";
 import Tbmd from "./TMDB";
 
 export default () => {
+  useEffect(() => {
+    const loadAll = async () => {
+      //Esse useEffect executa uma determinada função assim que a página é carregada
+      let list = await Tbmd.getHomeList();
+      console.log(list);
+    };
+    loadAll();
+    return () => {};
+  }, []);
   return <div>Uatiman</div>;
 };
--- src/TMDB.js
+++ src/TMDB.js
@@ -4,8 +4,8 @@
 //Os dados dos requests, usando um banco de dados feito por mim ou de outra API, eu apenas
 //PReciso editar este arquivo, pois ele irá centralizar tudo, tornando a minha vida muito
 //Mais fácil e deixando o desenvolvimento mais prático e organizado
-const API_KEY = "f42a31a6ad2f581ff02e34b50a18fa17"; //Chave da nossa API
-const API_BASE = "https://api.themoviedb.org/3"; //Base dos dados que iremos usar da API, encontradas nesse URL
+const API_KEY = 'f42a31a6ad2f581ff02e34b50a18fa17'; //Chave da nossa API
+const API_BASE = 'https://api.themoviedb.org/3'; //Base dos dados que iremos usar da API, encontradas nesse URL
 
 /*
 -originais Netflix  
@@ -45,7 +45,7 @@ export default {
         slug: "trending",
         title: "Recomendados para você",
         items: await basicFetch(
-          `trending/all/week?language=pt-BR&api_key= ${API_KEY}`
+          `/trending/all/week?language=pt-BR&api_key= ${API_KEY}`
         ),
       },
       {
@@ -59,35 +59,35 @@ export default {
         slug: "action",
         title: "Ação",
         items: await basicFetch(
-          `/discover/tv?with_network=28?language=pt-BR&api_key= ${API_KEY}`
+          `/discover/tv?with_genres=28?language=pt-BR&api_key= ${API_KEY}`
         ),
       },
       {
         slug: "terror",
         title: "Terror",
         items: await basicFetch(
-          `/discover/tv?with_network=27?language=pt-BR&api_key= ${API_KEY}`
+          `/discover/tv?with_genres=27?language=pt-BR&api_key= ${API_KEY}`
         ),
       },
       {
         slug: "comedy",
         title: "Comédia",
         items: await basicFetch(
-          `/discover/tv?with_network=35?language=pt-BR&api_key= ${API_KEY}`
+          `/discover/tv?with_genres=35?language=pt-BR&api_key= ${API_KEY}`
         ),
       },
       {
         slug: "romance",
         title: "Romance",
         items: await basicFetch(
-          `/discover/tv?with_network=10749?language=pt-BR&api_key= ${API_KEY}`
+          `/discover/tv?with_genres=10749?language=pt-BR&api_key= ${API_KEY}`
         ),
       },
       {
         slug: "documentary",
         title: "Documentário",
         items: await basicFetch(
-          `/discover/tv?with_network=99?language=pt-BR&api_key= ${API_KEY}`
+          `/discover/tv?with_genres=99?language=pt-BR&api_key= ${API_KEY}`
         ),
       },
     ];
