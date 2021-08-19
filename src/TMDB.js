7869f8d3334a9161e8c8912be4ca605a9405291d
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-19 20:26:41 -0300

    Arrumando mais erros de sintaxe

--- src/TMDB.js
+++ src/TMDB.js
@@ -59,35 +59,35 @@ export default {
         slug: "action",
         title: "Ação",
         items: await basicFetch(
-          `/discover/tv?with_genres=28?language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`
         ),
       },
       {
         slug: "terror",
         title: "Terror",
         items: await basicFetch(
-          `/discover/tv?with_genres=27?language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`
         ),
       },
       {
         slug: "comedy",
         title: "Comédia",
         items: await basicFetch(
-          `/discover/tv?with_genres=35?language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`
         ),
       },
       {
         slug: "romance",
         title: "Romance",
         items: await basicFetch(
-          `/discover/tv?with_genres=10749?language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`
         ),
       },
       {
         slug: "documentary",
         title: "Documentário",
         items: await basicFetch(
-          `/discover/tv?with_genres=99?language=pt-BR&api_key=${API_KEY}`
+          `/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`
         ),
       },
     ];
