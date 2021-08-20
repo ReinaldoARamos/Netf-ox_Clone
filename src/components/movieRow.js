63ba975689bb7f847cffc408b7a0f3aebce70bee
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-19 22:17:56 -0300

    Aqui eu vou criar uma linha com filmes que consegue se mover, para isso eu criei a div da row e coloquei para que caso o tamanho do array results(que vem la do movieList  for maior que 0, ou seja, ter itens, ele mapeie o array e crie um loop, e aqui eu irei passar uma sauce de imagem usando o API

--- src/components/movieRow.js
+++ src/components/movieRow.js
@@ -5,6 +5,11 @@ export default ({title, items}) => {
     return (
         <div>
             <h2>{title}</h2>
+            <div className= "movieRow--listarea"> 
+            {items.results.lenght > 0 && items.results.map((item, key) => (
+                <img src= "" />
+            ))}
+            </div>
         </div>
     )
 
