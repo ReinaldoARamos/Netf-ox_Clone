feb8486ae9bbca5b1806fa3174d1b8230a818b38
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-19 21:30:56 -0300

    Aqui eu criei um HTML para ser retornado, nesse caso ele pega a movielists(onde os valores foram previamente setados, faz um loop e pega o item titulo e retorna do obj

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, { useEffect, useState/*Salva a lista de filmes que iremos exibir*/ } from "react";
+import React, {
+  useEffect,
+  useState /*Salva a lista de filmes que iremos exibir*/,
+} from "react";
 import Tbmd from "./TMDB";
 
 export default () => {
-  const  [movielist, setMovieList] = useState([]); //Cpnst onde iremos setar os filmes
+  const [movielist, setMovieList] = useState([]); //Cpnst onde iremos setar os filmes
   useEffect(() => {
     const loadAll = async () => {
       //Esse useEffect executa uma determinada função assim que a página é carregada
       let list = await Tbmd.getHomeList();
-      console.log(list);
+      setMovieList(list);
     };
     loadAll();
     return () => {};
   }, []);
-  return <div>Uatiman</div>;
+  return (
+    <div className="page">
+      <section className="lists">
+        {movielist.map((item, key) => (
+          <div>{item.title}</div>
+        ))}
+      </section>
+    </div>
+  );
 };
