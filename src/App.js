b8aaf4704f6972936ac52cd234dccd60cef89bc6
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-19 21:12:30 -0300

    Estou criando uma conts onde irei setar os a lista de filmes usando o UseState, que salva a lista de filmes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { list } from "postcss";
-import React, { useEffect } from "react";
+import React, { useEffect, useState/*Salva a lista de filmes que iremos exibir*/ } from "react";
 import Tbmd from "./TMDB";
 
 export default () => {
+  const  [movielist, setMovieList] = useState([]); //Cpnst onde iremos setar os filmes
   useEffect(() => {
     const loadAll = async () => {
       //Esse useEffect executa uma determinada função assim que a página é carregada
