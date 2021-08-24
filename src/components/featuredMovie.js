d537ecdd3ac4cfa04718afd51a01ccdcda39c08b
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-24 19:16:16 -0300

    Eu coloquei todas as informações do filme em destaque e manipulei a data pra que retorne somente o ano, usando o método nativo new Date

--- src/components/featuredMovie.js
+++ src/components/featuredMovie.js
@@ -3,6 +3,8 @@ import "./FeaturedMovie.css";
 
 export default ({ item }) => {
     console.log(item)
+
+    let firtsDate = new Date(item.first_air_date);   
   return (
     <section
       className="featured"
@@ -17,10 +19,13 @@ export default ({ item }) => {
           <div className="featured--name"> {item.original_name}</div>
           <div className="featured--info">
             <div className="featured--points">{item.vote_average}</div>
-            <div className="featured--year">2099</div>
+            <div className="featured--year">{firtsDate.getFullYear()}</div>
             <div className="featured--seasons">{item.number_of_seasons}Temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
             
           </div>
+          <div className="featured--description">{item.overview }</div>
+          <div className="featured--buttons"></div>
+          <div className="featured--genres"><strong>Gêneros:</strong> ...</div>
         </div>
       </div>
     </section>
