f395cb4dd2702f4a34731cac618398b31fc602ce
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-24 19:03:20 -0300

    Terminando de colocar a informação das temporadas com um if condicional de que caso seja diferente de 1(numero de temporada) ele coloca o s, caso contrário ele tira

--- src/components/featuredMovie.js
+++ src/components/featuredMovie.js
@@ -18,6 +18,7 @@ export default ({ item }) => {
           <div className="featured--info">
             <div className="featured--points">{item.vote_average}</div>
             <div className="featured--year">2099</div>
+            <div className="featured--seasons">{item.number_of_seasons}Temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
             
           </div>
         </div>
