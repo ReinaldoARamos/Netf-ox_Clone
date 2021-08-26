ca627986e45566a14013de4dfed083f7d688f896
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-26 18:53:56 -0300

    Colocando a Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,10 @@ export default () => {
         <header>
             <div className="header--logo">
         <a href= "/">
-            <img src='https://assets.brand.microsites.netflix.io/assets/493f5bba-81a4-11e9-bf79-066b49664af6_cm_1440w.png?v=30' alt="Netflix" />
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png' alt="Netflix" />
         </a>
             </div>
-            <div className="headerUser">
+            <div className="header--User">
                 <a href= "/">
                     <img src="https://i.pinimg.com/236x/21/e1/09/21e109cb8cc83a54012113e656542813.jpg" />
                 </a>
