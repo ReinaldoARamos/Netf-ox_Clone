41f8f48d1a180b103e9c5491b91722b4af2dea7e
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-26 19:22:07 -0300

    Criando a estilização do Header com fundo cinza pra não sobrescrever os filmes, porém tenho que criar uma monitoração para ele saber se estou escrolando o mouse ou não

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 
 export default () => {
     return(
-        <header>
+        <header id="head" className="black">
             <div className="header--logo">
         <a href= "/">
             <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png' alt="Netflix" />
