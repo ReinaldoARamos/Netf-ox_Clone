e587cdcd9c1ef0aea139c463761241d92dc6f935
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-26 21:31:12 -0300

    Criei com sucesso a função que faz o scroll pra direita, também criei e verificação que impede de eu mandar os filmes pro lado infinitamente

--- src/components/movieRow.js
+++ src/components/movieRow.js
@@ -1,24 +1,25 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./MovieRow.css";
 import { NavigateBefore } from "@material-ui/icons";
 import { NavigateNext } from "@material-ui/icons";
 
 export default ({ title, items }) => {
-
   const [scrollX, setScrollX] = useState(0);
   const handleLeftArrow = () => {
     let x = scrollX + Math.round(window.innerWidth / 2); //Pega o valor do monitor do usuário e divide por 2
-    if(x>0){ //verificação de que caso o x for maior que 0, ele iguale o x a 0, impedindo de sair do limite
-      x=0
+    if (x > 0) {
+      //verificação de que caso o x for maior que 0, ele iguale o x a 0, impedindo de sair do limite
+      x = 0;
     }
-setScrollX(x); //setamos o valor
+    setScrollX(x); //setamos o valor
   };
   const handleRightArrow = () => {
     let x = scrollX - Math.round(window.innerWidth / 2); //
-    if(x>0){ //verificação de que caso o x for maior que 0, ele iguale o x a 0, impedindo de sair do limite
-      x=0
+    let ListW = items.results.length * 200; //Isso vai pegar o tamanho da lista
+    if (window.innerWidth - ListW > x) { //Aqui vai verificar se a diferença entre o tamanho da lista e a tela do usuário sao menores que o x
+      x = (window.innerWidth - ListW) - 60; //caso seja, ele vai dimiuir ambos e dps diminuir 60, que é o padding da lista(30 na esquerda e 30 na direita)
     }
-setScrollX(x); //setamos o valor
+    setScrollX(x); //setamos o valor
   };
   return (
     <div className="movieRow">
@@ -28,10 +29,13 @@ setScrollX(x); //setamos o valor
       </div>
 
       <div className="movieRow--right" onClick={handleRightArrow}>
-        <NavigateNext style={{ fontSize: 50 }} /> 
+        <NavigateNext style={{ fontSize: 50 }} />
       </div>
-      <div className="movieRow--listarea" >
-        <div className="movieRow--list" style={{marginLeft: scrollX, width: items.results.length * 200 }}>
+      <div className="movieRow--listarea">
+        <div
+          className="movieRow--list"
+          style={{ marginLeft: scrollX, width: items.results.length * 200 }}
+        >
           {items.results.length > 0 &&
             items.results.map((item, key) => (
               <div key={key} className="movieRow--item">
