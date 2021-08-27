348bbbde31a2b1662224ee3d6964c86250935a2e
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2021-08-26 22:05:38 -0300

    Adicionando um limitador de caractéres...e...PROJETO FINALIZADO

--- src/components/featuredMovie.js
+++ src/components/featuredMovie.js
@@ -10,6 +10,10 @@ export default ({ item }) => {
         genres.push(item.genres[i].name);  
         
     }
+    let description = item.overview;
+    if(description.length > 200){
+      description = description.substring(0, 200) + "..."
+    }
   return (
     <section
       className="featured"
@@ -28,7 +32,7 @@ export default ({ item }) => {
             <div className="featured--seasons">{item.number_of_seasons} Temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
             
           </div>
-          <div className="featured--description">{item.overview }</div>
+          <div className="featured--description">{description }</div>
           <div className="featured--buttons">
               <a href= {`/watch/${item.id}`} className="featured--watchBTN">► Assistir</a>
               <a href= {`/list/add/${item.id}`} className="featured--listBTN">ⓘ Mais Informações</a>
